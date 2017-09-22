cb8e103fa442a930a916714f10b5f747bf9acc9d
wangxiaodan <wangxiaodan@hsohealth.com>
2017-09-22 22:48:27 +0800

    支付页面

--- src/container/PayPage.js
+++ src/container/PayPage.js
@@ -3,13 +3,23 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as Immutable from 'immutable';
 import * as styles from '../assets/stylesheets/dashList.css';
+import * as payStyle from '../assets/stylesheets/payPage.css';
 import Boy from '../assets/images/boy.svg';
 import Girl from '../assets/images/girl.svg';
+import { List, Radio, Flex, WhiteSpace } from 'antd-mobile';
+
+const RadioItem = Radio.RadioItem;
 
 class PayPage extends React.PureComponent {
   static propTypes = {
     openid: PropTypes.string,
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: true,
+    };
+  }
   render() {
     const showNum = (num) => {
       if(num > 0) {
@@ -21,21 +31,39 @@ class PayPage extends React.PureComponent {
     return (
       <div style={{ backgroundColor: '#EEEEEE', height: '93vh'}} >
           {/*pay title*/}
-         <div>
-               <div className={styles.dashNum}>
+         <div className={payStyle.payTitle}>
+               <div className={payStyle.dashNum}>
                   <Boy className={styles.dashImg}/>
-                  <div className={styles.dashBoy}>男士：{showNum(dashItem.boyNum)}</div>
+                  <div className={payStyle.dashBoy}>男士：{showNum(dashItem.boyNum)}</div>
                   <Girl className={styles.dashImg}/>
                   <div>女士：{showNum(dashItem.girlNum)}</div>
                </div>
-              <div></div>
+               <div className={payStyle.payText}>支付剩余时间</div>
+               <div className={payStyle.payNum}><span>30</span>S</div>
          </div>
          {/*pay content*/}
-         <div></div>
+         <div className={payStyle.payContent}>
+           <div className={payStyle.dashHeader}>
+              <img src={'https://img.shaka.hsohealth.com/insurance/diet_banner_4_20170206.png'} className={payStyle.payImg} />
+               <div>
+                   <div className={payStyle.money}>￥100</div>
+                   <div className={payStyle.dashTitle}>活动标题！</div>
+               </div>
+           </div>
+         </div>
          {/*pay type*/}
-         <div></div>
+         <div className={payStyle.selectType}>
+           <RadioItem checked={this.state.checked} onChange={() => {
+               const checked = this.state.checked;
+               this.setState({ checked: !checked });
+           }}>
+             微信支付
+           </RadioItem>
+         </div>
          {/*pay button*/}
-         <div></div>
+         <div className={payStyle.payButton}>
+              确认支付  ￥100
+         </div>
       </div>
     );
   }
