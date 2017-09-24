233f16f41108b8823f2ca34a4b3e8df3caa4a6fb
wl <wl@WangLus-MacBook-Pro.local>
2017-09-24 23:45:00 +0800

    feat(Component): 编辑个人信息

--- src/core/RoutingURL/RoutingURL.js
+++ src/core/RoutingURL/RoutingURL.js
@@ -9,4 +9,4 @@ export const PayPage = ():string => '/pay-page'
 export const Mine = (): string => '/mine';
 // 个人资料
 // export const BLPrefix = (prefixs: string): string =>`/blood-lipids-management/${prefixs}`;
-export const UserInfo = (edit): string => `/user-info${edit}`;
+export const UserInfo = (tab): string => `/user-info${tab}`;
