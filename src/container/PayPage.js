092253c2e48c97f27a771053f28dca0ea6035036
王晓丹 <wangxiaodan@192.168.0.111>
2017-10-05 17:55:11 +0800

    bug fix

--- src/container/PayPage.js
+++ src/container/PayPage.js
@@ -56,6 +56,7 @@ class PayPage extends React.PureComponent {
     }
     const dashItem = JSON.parse(JSON.stringify(this.props.dashInfo));
     const sex = dashItem.sex;
+    altert(sex);
     return (
       <div>
       <div style={{ backgroundColor: '#EEEEEE', height: 'calc(100vh - 14vw)'}} >
