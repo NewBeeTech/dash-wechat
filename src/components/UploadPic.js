3a9da59e4e53ebb768359f8361b19eda02fc4689
wl <wl@WangLus-MacBook-Pro.local>
2017-10-05 15:50:35 +0800

    feat:

--- src/components/UploadPic.js
+++ src/components/UploadPic.js
@@ -22,11 +22,9 @@ class UploadPic extends React.Component {
       // console.log(fileInfo);
       if (fileInfo.fileURL) {
         const photos = this.props.photos;
-        console.log('11111', photos);
         photos[index] = fileInfo.fileURL;
         // 上传成功的图片显示
         this.props.setStatePhotos(photos);
-        console.log('22222', photos);
       } else {
         // 上传失败的图片显示
         Toast.info('上传失败，请稍后再试');
@@ -58,7 +56,6 @@ class UploadPic extends React.Component {
               className={styles.uploadButton2}
               style={this.props.photos[2] ? {backgroundSize: 'cover', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[2]})`, marginTop: '1vw'} : {marginTop: '1vw'}}
             >
-              {console.log(this.props.photos[2])}
               {this.props.photos[2] ? '' : <img width="20%" src="http://dash.oss-cn-beijing.aliyuncs.com/fe/add_pic.png"/>}
             </div>
           </Upload>
