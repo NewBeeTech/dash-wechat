6f19c16a3bf79cd4187c4da0ac5cf47930f3f50f
huliyou <liyoupie@gmail.com>
2017-10-06 16:35:53 +0800

    fix(try to fix, fail):

--- src/components/UploadPic.js
+++ src/components/UploadPic.js
@@ -15,12 +15,44 @@ type Props = {
 };
 class UploadPic extends React.Component {
   props: Props;
-  beforeUpload(file, index) {
+  beforeUploadFirst(file) {
+    const result = UploadFileToOSS(file);
+    alert(file.name);
+    result.then(fileInfo => {
+      if (fileInfo.fileURL) {
+        const photos = this.props.photos;
+        photos[0] = fileInfo.fileURL;
+        alert(fileInfo.fileURL);
+        // 上传成功的图片显示
+        this.props.setStatePhotos(photos);
+      } else {
+        // 上传失败的图片显示
+        Toast.info('上传失败，请稍后再试');
+      }
+    });
+  }
+  beforeUploadSecond(file) {
     const result = UploadFileToOSS(file);
     result.then(fileInfo => {
       if (fileInfo.fileURL) {
         const photos = this.props.photos;
-        photos[index] = fileInfo.fileURL;
+        photos[1] = fileInfo.fileURL;
+        // 上传成功的图片显示
+        this.props.setStatePhotos(photos);
+      } else {
+        // 上传失败的图片显示
+        Toast.info('上传失败，请稍后再试');
+      }
+    });
+  }
+  beforeUploadThird(file) {
+    alert(`fileName: ${file.name}`);
+    alert(`index: ${index}`);
+    const result = UploadFileToOSS(file);
+    result.then(fileInfo => {
+      if (fileInfo.fileURL) {
+        const photos = this.props.photos;
+        photos[2] = fileInfo.fileURL;
         // 上传成功的图片显示
         this.props.setStatePhotos(photos);
       } else {
@@ -30,9 +62,28 @@ class UploadPic extends React.Component {
     });
   }
   render() {
+    const uploadProps = {
+      beforeUpload: (file) => {
+        const result = UploadFileToOSS(file);
+        alert(file.name);
+        result.then(fileInfo => {
+          if (fileInfo.fileURL) {
+            alert(this);
+            const photos = this.props.photos;
+            photos[0] = fileInfo.fileURL;
+            alert(fileInfo.fileURL);
+            // 上传成功的图片显示
+            this.props.setStatePhotos(photos);
+          } else {
+            // 上传失败的图片显示
+            Toast.info('上传失败，请稍后再试');
+          }
+        });
+      }
+    }
     return (
       <div className={styles.uploadPic}>
-        <Upload beforeUpload={(file) => this.beforeUpload(file, 0)} ref="inner">
+        <Upload { ...uploadProps } ref="inner">
           <div
             className={styles.uploadButton}
             style={this.props.photos[0] ? {backgroundSize: 'cover', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[0]})` } : {}}
@@ -41,7 +92,7 @@ class UploadPic extends React.Component {
           </div>
         </Upload>
         <div>
-          <Upload beforeUpload={(file) => this.beforeUpload(file, 1)} ref="inner">
+          <Upload beforeUpload={(file) => this.beforeUploadSecond(file)} ref="inner">
             <div
               className={styles.uploadButton2}
               style={this.props.photos[1] ? {backgroundSize: 'cover', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[1]})`} : {}}
@@ -49,7 +100,7 @@ class UploadPic extends React.Component {
               {this.props.photos[1] ? '' : <img width="20%" src="http://dash.oss-cn-beijing.aliyuncs.com/fe/add_pic.png"/>}
             </div>
           </Upload>
-          <Upload beforeUpload={(file) => this.beforeUpload(file, 2)} ref="inner">
+          <Upload beforeUpload={(file) => this.beforeUploadThird(file)} ref="inner">
             <div
               className={styles.uploadButton2}
               style={this.props.photos[2] ? {backgroundSize: 'cover', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[2]})`, marginTop: '1vw'} : {marginTop: '1vw'}}
