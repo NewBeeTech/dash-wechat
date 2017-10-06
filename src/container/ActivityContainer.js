570134fec195c7d31994c34e15a821addb39c793
wangxiaodan <wangxiaodan@hsohealth.com>
2017-10-07 00:57:54 +0800

    bug fix

--- src/container/ActivityContainer.js
+++ src/container/ActivityContainer.js
@@ -45,7 +45,7 @@ class ActivityContainer extends React.PureComponent {
     // 获取活动详情
     this.props.dispatch(ActivityAction.getDashInfoData({activityId: this.props.params.activityId}));
     // 获取患者在该活动的状态
-    this.props.dispatch(ActivityAction.getUserForDashData({activityId: this.props.params.activityId}));
+    // this.props.dispatch(ActivityAction.getUserForDashData({activityId: this.props.params.activityId}));
 
     this.setState({
       isSignUp: this.props.isSignUp,
@@ -72,7 +72,7 @@ class ActivityContainer extends React.PureComponent {
     const isSignUp = this.state.isSignUp; // 1失败 0未支付 1成功 2运营拒绝 3用户取消
     const signNum = this.state.signNum;
     const sex = this.props.userData.get('userInfo').get('sex');
-    const status = this.props.userData.get('userInfo').get('status');
+    const UserStatus = this.props.userData.get('userInfo').get('status');
     let buttonText = '报名';
     let status = true;
     if(isSignUp == 0 && ((sex == 1 && dashInfo.get('boyNum') == signNum) || (sex == 2 && dashInfo.get('grilNum') == signNum))) {
