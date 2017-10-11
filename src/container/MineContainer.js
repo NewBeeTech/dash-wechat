a3022c02bd8340fbfe7c888eb63668b5f6ae545b
wl <wl@WangLus-MacBook-Pro.local>
2017-10-11 16:18:23 +0800

    fix:

--- src/container/MineContainer.js
+++ src/container/MineContainer.js
@@ -47,7 +47,7 @@ class MineContainer extends React.Component {
             />
           </div>
           <div className={styles.creditCount}>
-              <div className={styles.panelHeader}><img src="./../assets/images/xinyong.png" />&nbsp;&nbsp;信用记录</div>
+              <div className={styles.panelHeader}><img src="http://dash.oss-cn-beijing.aliyuncs.com/fe/xinyong.png" />&nbsp;&nbsp;信用记录</div>
               {/* <div>信用记录</div> */}
               <div>{this.props.userInfo.get('creditCount')}</div>
           </div>
