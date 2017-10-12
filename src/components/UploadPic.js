4c771fc247bb02869df54e45ad0fe5682775e422
wl <wl@WangLus-MacBook-Pro.local>
2017-10-12 11:42:02 +0800

    fix:

--- src/components/UploadPic.js
+++ src/components/UploadPic.js
@@ -8,7 +8,7 @@ import { Toast } from 'antd-mobile';
 import Upload from 'rc-upload';
 import * as styles from './../assets/stylesheets/mine.css'
 import { UploadFileToOSS } from './../core/WS/WSHandler';
-const FileInput = require('react-file-input');
+
 
 type Props = {
   photos: Array,
@@ -62,6 +62,13 @@ class UploadPic extends React.Component {
       }
     });
   }
+  renderUpload(index) {
+    return (
+      <div>
+
+      </div>
+    )
+  }
   render() {
     // const uploadProps = {
     //   beforeUpload: (file) => {
@@ -83,17 +90,15 @@ class UploadPic extends React.Component {
     //   }
     // }
     const handleChange = (event, index) => {
-      console.log(index);
       const file =  event.target.files[0];
-      console.log('Selected file:', event.target.files[0]);
       const result = UploadFileToOSS(file);
-      alert(file.name);
+      if((file.type).indexOf("image/") === -1){
+        return Toast.info("请上传图片", 2);
+      }
       result.then(fileInfo => {
         if (fileInfo.fileURL) {
-          alert(this);
           const photos = this.props.photos;
           photos[index] = fileInfo.fileURL;
-          alert(fileInfo.fileURL);
           // 上传成功的图片显示
           this.props.setStatePhotos(photos);
         } else {
@@ -109,7 +114,7 @@ class UploadPic extends React.Component {
           style={this.props.photos[0] ? {backgroundSize: 'contain', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[0]})`, backgroundPosition: 'center center' } : {}}
         >
           {this.props.photos[0] ? '' : <img width="20%" src="http://dash.oss-cn-beijing.aliyuncs.com/fe/add_pic.png"/>}
-          <input type="file" accept=".png,.gif,.jpg" onChange={(e) => handleChange(e, 0)} />
+          <input type="file" accept="" onChange={(e) => handleChange(e, 0)} />
         </div>
         {/* <Upload { ...uploadProps } ref="inner">
           <div
@@ -126,7 +131,7 @@ class UploadPic extends React.Component {
               style={this.props.photos[1] ? {backgroundSize: 'contain', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[1]})`, backgroundPosition: 'center center'} : {}}
             >
               {this.props.photos[1] ? '' : <img width="20%" src="http://dash.oss-cn-beijing.aliyuncs.com/fe/add_pic.png"/>}
-              <input type="file" accept=".png,.gif,.jpg" onChange={(e) => handleChange(e, 1)} />
+              <input type="file" accept="" onChange={(e) => handleChange(e, 1)} />
             </div>
           {/* </Upload> */}
           {/* <Upload beforeUpload={(file) => this.beforeUploadThird(file)} ref="inner"> */}
@@ -135,7 +140,7 @@ class UploadPic extends React.Component {
               style={this.props.photos[2] ? {backgroundSize: 'contain', backgroundRepeat: 'no-repeat',  backgroundImage: `url(${this.props.photos[2]})`, backgroundPosition: 'center center', top: '1vw'} : {top: '1vw'}}
             >
               {this.props.photos[2] ? '' : <img width="20%" src="http://dash.oss-cn-beijing.aliyuncs.com/fe/add_pic.png"/>}
-              <input type="file" accept=".png,.gif,.jpg" onChange={(e) => handleChange(e, 2)} />
+              <input type="file" accept="" onChange={(e) => handleChange(e, 2)} />
             </div>
           {/* </Upload> */}
         </div>
