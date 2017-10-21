104e81e1ff181f342113972bdb093b09f4a191f8
deerW <deerW@deerw.local>
2017-10-21 15:12:19 +0800

    fix:

--- src/components/UserActivityList.js
+++ src/components/UserActivityList.js
@@ -51,7 +51,7 @@ class UserActivityList extends React.Component {
     const statusColor = { 1: '#ffce3d', 2: '#f40',  3: '#999', 4: '#ffce3d', 5: '#999', 6: '#999' };
     const info = { 1: 'done', 2: 'primary',  3: 'cancel', 4: 'primary',  5: 'primary', 6: 'primary' };
     list.map((item, index) => {
-      console.log(index, moment().format('X'), moment(item.get('startTime')).format('X'));
+      // console.log(index, moment().format('X'), moment(item.get('startTime')).format('X'));
       if(status.indexOf(item.get('status')) > -1 &&
       ((type === 1 && (moment(item.get('startTime')).format('X') > moment().format('X') || item.get('status') == 3)) ||
       (type === 2 && moment(item.get('startTime')).format('X') < moment().format('X')))) {
@@ -103,7 +103,7 @@ class UserActivityList extends React.Component {
           style={{ height: '34vw', position: 'relative' }}
           thumb={this.renderImg(item.get('photos'))}
           multipleLine
-          onClick={() => {this.props.routeToActivity(item.get('activityId'), info)}}
+          onClick={() => {this.props.routeToActivity(item.get('activityId'))}}
         >
           <span style={{ fontSize: '4vw' }}>{item.get('activityName')}</span>
           <List.Item.Brief style={{ fontSize: '3.5vw', color: '#999' }}>
